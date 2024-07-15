d77b0275aa09f8e3cfdfd431a2a44f1187b8ee76
Hugo Silva <yhugorocha@gmail.com>
2024-07-15 08:28:58 -0300

    FEAT: Initial commit.

---
+++ src/main/java/com/hugodev/upload_arquivos_api/UploadArquivosApiApplication.java
@@ -0,0 +1,13 @@
+package com.hugodev.upload_arquivos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadArquivosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadArquivosApiApplication.class, args);
+	}
+
+}
